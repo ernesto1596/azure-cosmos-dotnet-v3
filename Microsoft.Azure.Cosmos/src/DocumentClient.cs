e92fb6b32e14684b8bffd1625d3b6925fbc11477
Sourabh Jain <sourabhjain@microsoft.com>
2023-01-17 07:08:01 +0530

    error handling

--- Microsoft.Azure.Cosmos/src/DocumentClient.cs
+++ Microsoft.Azure.Cosmos/src/DocumentClient.cs
@@ -6441,7 +6441,17 @@ namespace Microsoft.Azure.Cosmos
                 return request;
             }
 
-            return await gatewayModel.GetDatabaseAccountClientConfigAsync(CreateRequestMessage);
+            try
+            {
+                return await gatewayModel.GetDatabaseAccountClientConfigAsync(CreateRequestMessage);
+            }
+            catch (Exception ex)
+            {
+                DefaultTrace.TraceError($"Error while fetching client configuration. Sevice Endpoint: {serviceEndpoint}, Exception: {ex.StackTrace}");
+            }
+
+            //In case of any exception, return this as null.
+            return null;
         }
 
         /// <summary>
